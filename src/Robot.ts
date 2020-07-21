71d1ec02ea677343362bf1db9ec3be99c8903327
mmarifat <mma.rifat66@gmail.com>
2020-07-21 11:41:41 +0600

    comments

--- src/Robot.ts
+++ src/Robot.ts
@@ -1,13 +1,13 @@
 import * as robot from "robotjs"
 
-let comments: Array<string> = ['Hello', 'Auto', 'Comments', 'Using RobotJS', 'Whats', 'Up']
+let texts: Array<string> = ['Hello', 'Auto', 'Comments', 'Using RobotJS', 'Whats', 'Up']
 
-// you have 5 seconds to select where to put comment (Change it you need more time)
+// you have 5 seconds to select where to put text (Change it you need more time)
 robot.setMouseDelay(5 * 1000)
 robot.mouseClick("left")
 
-comments.forEach(each => {
-	// wait 3 seconds for each comment to prove that human is commenting
+texts.forEach(each => {
+	// wait 3 seconds for each text to prove that human is typing
 	robot.setKeyboardDelay(3 * 1000)
 	robot.typeString(each)
 	robot.keyTap('enter')
